7ea12df9c909785c75e324183942c7ef2d917bce
yasiruTJ <yasiejayatissa@gmail.com>
2022-04-11 04:58:59 +0530

    minor change

--- lib/male.dart
+++ lib/male.dart
@@ -21,91 +21,93 @@ class Male extends StatelessWidget {
         backgroundColor: Colors.grey[900],
         foregroundColor: Colors.white,
       ),
-      body: GestureDetector(
-        onTap: (){
-          FocusScope.of(context).unfocus();
-        },
-        child: SingleChildScrollView(
-          child: Center(
-            child: Container(
-              height: 1000,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/back.jpg"),
-                    fit: BoxFit.cover),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(0.0,60.0, 0.0, 0.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        CircleAvatar(
-                          backgroundColor: Colors.white,
-                          backgroundImage: AssetImage('images/male.png'),
-                          radius: 80.0,
+      body: InkWell(
+        child: GestureDetector(
+          onTap: (){
+            FocusScope.of(context).unfocus();
+          },
+          child: SingleChildScrollView(
+            child: Center(
+              child: Container(
+                height: 1000,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/back.jpg"),
+                      fit: BoxFit.cover),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0,60.0, 0.0, 0.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          CircleAvatar(
+                            backgroundColor: Colors.white,
+                            backgroundImage: AssetImage('images/male.png'),
+                            radius: 80.0,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20.0,),
+                      const TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Name',
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: UnderlineInputBorder(),
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 20.0,),
-                    const TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Name',
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: UnderlineInputBorder(),
+                        keyboardType: TextInputType.text,
                       ),
-                      keyboardType: TextInputType.text,
-                    ),
-                    const SizedBox(height: 15.0,),
-                    const TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Age',
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: UnderlineInputBorder(),
+                      const SizedBox(height: 15.0,),
+                      const TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Age',
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: UnderlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.number,
                       ),
-                      keyboardType: TextInputType.number,
-                    ),
-                    const SizedBox(height: 15.0,),
-                    const TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Height',
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: UnderlineInputBorder(),
+                      const SizedBox(height: 15.0,),
+                      const TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Height',
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: UnderlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.text,
                       ),
-                      keyboardType: TextInputType.text,
-                    ),
-                    const SizedBox(height: 15.0,),
-                    const TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Weight',
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: UnderlineInputBorder(),
+                      const SizedBox(height: 15.0,),
+                      const TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Weight',
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: UnderlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.text,
                       ),
-                      keyboardType: TextInputType.text,
-                    ),
-                    const SizedBox(height: 20.0,),
-                    FlatButton(
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => CreateAccount()));
-                      },
-                      color: Colors.grey[900],
-                      child: const Text(
-                        'Next',
-                         style: TextStyle(
-                         fontSize: 20.0,
-                         color: Colors.white,
-                         fontWeight: FontWeight.bold,
-                         fontFamily: 'Oswald',
+                      const SizedBox(height: 20.0,),
+                      FlatButton(
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => CreateAccount()));
+                        },
+                        color: Colors.grey[900],
+                        child: const Text(
+                          'Next',
+                           style: TextStyle(
+                           fontSize: 20.0,
+                           color: Colors.white,
+                           fontWeight: FontWeight.bold,
+                           fontFamily: 'Oswald',
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
